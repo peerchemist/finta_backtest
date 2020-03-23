c1de7d8a715cb82b781f42018530880600b2be5c
Peerchemist <dx.1178@gmail.com>
2020-03-23 16:58:39 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+from os import path
+
+classifiers = [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Financial and Insurance Industry",
+    "Programming Language :: Python",
+    "Operating System :: OS Independent",
+    "Natural Language :: English",
+    "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+]
+
+# read the contents of your README file
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="finta_backtest",
+    version="0.0.1",
+    description="Backtest trading strategies, based on the finta library.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=["technical analysis", "backtesting", "ta", "pandas", "finance", "numpy", "analysis"],
+    url="https://github.com/peerchemist/finta_backtest",
+    author="Peerchemist",
+    author_email="peerchemist@protonmail.ch",
+    license="LGPLv3+",
+    packages=["finta_backtest"],
+    install_requires=["finta"],
+)
